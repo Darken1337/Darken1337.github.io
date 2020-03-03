808d455a8f3dd2c847ba483531ae4bbe9211df58
Darken1337 <40112493+Darken1337@users.noreply.github.com>
2020-03-03 13:08:09 +0300

    1.0

--- nyavchuck/static/js/main.js
+++ nyavchuck/static/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
+    
+    //Modals
     var modalsInit = {
         fadeTime: 400,
         currentModal: null,
@@ -44,6 +45,57 @@ $(document).ready(function(){
     
     })
 
+    //Маска для телефона
     $('[data-mask="phone"]').mask('+380?(99)999-99-99', {autoclear: false});
+    
+
+    //Меню мобильное
+    $('#hamb').on('click', function(){
+
+        $('#menu-mobile').addClass('is-active')
+
+    })
+    $('#close-menu').on('click', function(){
+
+        $('#menu-mobile').removeClass('is-active')
+
+    })
+
+    //Слайдер
+
+    if($(window).width() < 1024){
+        var slick = $('.js-slider-testimonials').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            infinite: true,
+            useCSS: true,
+            useTransform: true,
+            responsive: [
+                {
+                  breakpoint: 767,
+                  settings: {
+                      dots: true,
+                      appendDots: $('.testimonials__dots')
+                  }
+                }
+              ]
+        });
+
+        //setHeight to slider elements
+
+        var slickElements = $('.testimonial'),
+            elementHeights = [];
+
+        slickElements.each(function(){
+
+            elementHeights.push($(this).outerHeight());
+
+        })
+
+        var maxValue = Math.max.apply(null, elementHeights);
+
+        slickElements.css('height', maxValue)
+    }
 
 })
