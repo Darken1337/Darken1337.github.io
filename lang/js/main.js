7f3368830ef2edc6628c797cf84fd517380cbc1a
Darken1337 <kreker23456@gmail.com>
2021-04-05 15:32:30 +0300

    32

--- lang/js/main.js
+++ lang/js/main.js
@@ -75,6 +75,30 @@ jQuery(document).ready(function($){
         }
         
     })
+    // ************* init spot animation *************
+    var radius = 8;
+    TweenMax.staggerFromTo('.blob', 5 ,{
+        cycle: {
+            attr:function(i) {
+                var r = i*90;
+                return {
+                    transform:'rotate('+r+') translate('+radius+',0.1) rotate('+(-r)+')'
+                }
+            }
+        }
+    },{
+        cycle: {
+            attr:function(i) {
+                var r = i*90+360;
+                return {
+                    transform:'rotate('+r+') translate('+radius+',0.1) rotate('+(-r)+')'
+                }
+            }
+        },
+        ease:Linear.easeNone,
+        repeat:-1
+    });
+    // **************************************
 
     updateDevice();
     menuInit();
